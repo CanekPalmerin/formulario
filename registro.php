3c3b6160cfb7761ef6a7a4ce6c179ee0b9ad9665
José Emilio Canek Palmerin Rubio <you@example.com>
2021-04-27 21:51:28 -0500

    version1

---
+++ registro.php
@@ -0,0 +1,15 @@
+<?php
+  $realname = $_POST['realname'];
+  $nick = $_POST['nick'];
+  $nick = $_POST['email'];
+  $nick = $_POST['direccion'];
+  $nick = $_POST['celular'];
+  $nick = $_POST['tel'];
+  $nick = $_POST['RFC'];
+  $nick = $_POST['CURP'];
+  $pass = $_POST['pass'];
+  $rpass = $_POST['rpass'];
+  $reqlen = strlen($nick) * strlen($pass) *strlen ($rpass);
+  
+  
+?>
